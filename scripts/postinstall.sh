6398cfd008872f39b408fdd531e5b1cda3d57a62
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2017-12-21 11:44:31 -0500

    Build dotenv

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,6 +1,8 @@
 # Add podspecs
 git apply --directory=node_modules/react-native-navigation ./scripts/patches/react-native-navigation.patch
 
+source "$(dirname "$0")/build-env.sh"
+
 # Pod install
 if [ -z "$TRAVIS" ]; then
   (cd ios; pod install; cd -)
