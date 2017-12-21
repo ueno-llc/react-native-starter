53342f1f3c6eb961a9063b02596090e9657566d4
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2017-12-21 11:55:39 -0500

    Build tools

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,7 +1,7 @@
+./scripts/build-env.sh
+
 # Add podspecs
 git apply --directory=node_modules/react-native-navigation ./scripts/patches/react-native-navigation.patch
-
-source "$(dirname "$0")/build-env.sh"
 
 # Pod install
 if [ -z "$TRAVIS" ]; then
