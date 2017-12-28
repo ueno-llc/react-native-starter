21da5dae640c387dd1531a79bb66ab7c59a757d0
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2017-12-28 14:15:01 -0500

    Placeholder files

--- ios/react-native-starter/AppDelegate.m
+++ ios/react-native-starter/AppDelegate.m
@@ -22,20 +22,26 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   [RNSentry installWithBridge:[[RCCManager sharedInstance] getBridge]];
-  [FIRApp configure];
+
+  if ([[NSDictionary dictionaryWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"GoogleService-Info" ofType:@"plist"]] objectForKey:@"GOOGLE_APP_ID"]) {
+    [FIRApp configure];
+    NSLog(@"Firebase initialized");
+  } else {
+    NSLog(@"Warning: Invalid GoogleService-Info.plist. Get one from the Firebase Console.");
+  }
 
   NSURL *jsCodeLocation;
-  
+
 #ifdef DEBUG
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
 #else
   jsCodeLocation = [CodePush bundleURL];
 #endif
-  
+
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   self.window.backgroundColor = [UIColor whiteColor];
   [[RCCManager sharedInstance] initBridgeWithBundleURL:jsCodeLocation launchOptions:launchOptions];
-  
+
   return YES;
 }
 
