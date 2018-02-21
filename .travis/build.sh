1b3a9e9cf8d285a02d76cc81838e6a3dbf470ba0
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2018-02-21 08:29:37 -0500

    Bump version number to allow build to pass

--- .travis/build.sh
+++ .travis/build.sh
@@ -27,12 +27,20 @@ if [[ "$TRAVIS_BRANCH" == "master" ]]; then
 
   if [[ "$LANE" == "ios" && "$TRAVIS_BUILD_IOS" == "1" ]]; then
     cd ios
-    fastlane travis || exit $?
+    if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+      fastlane travis || exit $?
+    else
+      fastlane build || exit $?
+    fi
   fi
 
   if [[ "$LANE" == "android" && "$TRAVIS_BUILD_ANDROID" == "1" ]]; then
     cd android
-    fastlane travis || exit $?
+    if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+      fastlane travis || exit $?
+    else
+      fastlane build || exit $?
+    fi
   fi
 
   if [[ "$LANE" == "js" ]]; then
@@ -42,22 +50,25 @@ if [[ "$TRAVIS_BRANCH" == "master" ]]; then
     # Test code
     yarn test || exit $?
 
-    # Install code-push-cli
-    npm install -g code-push-cli || exit $?
+    if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
 
-    # Login to code-push-cli
-    code-push login --accessKey $CODEPUSH_ACCESS_KEY || exit $?
+      # Install code-push-cli
+      npm install -g code-push-cli || exit $?
 
-    if [[ "$TRAVIS_BUILD_IOS" == "0" ]]; then
-      echo "Releasing code-push for iOS"
-      code-push release-react $IOS_CODEPUSH_APPID ios --outputDir build --description "$TRAVIS_COMMIT_MESSAGE"  --plistFile ./ios/react-native-starter/Info.plist
-      sentry-cli react-native codepush $IOS_CODEPUSH_APPID ios ./build --bundle-id $IOS_BUNDLE_ID
-    fi
+      # Login to code-push-cli
+      code-push login --accessKey $CODEPUSH_ACCESS_KEY || exit $?
 
-    if [[ "$TRAVIS_BUILD_ANDROID" == "0" ]]; then
-      echo "Releasing code-push for Android"
-      code-push release-react $ANDROID_CODEPUSH_APPID android --outputDir build --description "$TRAVIS_COMMIT_MESSAGE"
-      sentry-cli react-native codepush $ANDROID_CODEPUSH_APPID android ./build --bundle-id $ANDROID_BUNDLE_ID
+      if [[ "$TRAVIS_BUILD_IOS" == "0" ]]; then
+        echo "Releasing code-push for iOS"
+        code-push release-react $IOS_CODEPUSH_APPID ios --outputDir build --description "$TRAVIS_COMMIT_MESSAGE"  --plistFile ./ios/react-native-starter/Info.plist
+        sentry-cli react-native codepush $IOS_CODEPUSH_APPID ios ./build --bundle-id $IOS_BUNDLE_ID
+      fi
+
+      if [[ "$TRAVIS_BUILD_ANDROID" == "0" ]]; then
+        echo "Releasing code-push for Android"
+        code-push release-react $ANDROID_CODEPUSH_APPID android --outputDir build --description "$TRAVIS_COMMIT_MESSAGE"
+        sentry-cli react-native codepush $ANDROID_CODEPUSH_APPID android ./build --bundle-id $ANDROID_BUNDLE_ID
+      fi
     fi
   fi
 fi
