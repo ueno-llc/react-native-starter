f2c388ba323d876d5f77b02e30ad10d2c9e88226
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2018-08-17 14:47:30 -0400

    RN57 with extended metro config

---
+++ metro.config.js
@@ -0,0 +1,11 @@
+const { mergeConfig } = require('metro-config');
+const { DEFAULT } = require('react-native/local-cli/util/Config');
+
+const config = {
+  resolver: {
+    sourceExts: ['js', 'json', 'ts', 'tsx', 'css'],
+  },
+  transformModulePath: require.resolve('./scripts/transformer.js'),
+};
+
+module.exports = mergeConfig(DEFAULT, config);
