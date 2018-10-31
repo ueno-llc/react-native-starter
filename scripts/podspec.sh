8bbeca3be5e1d2b298e032b0cd25542998ac23ca
Jérémy Barbet <jeremgraph@gmail.com>
2018-10-31 16:10:58 +0000

    Improve and fix podspec script

--- scripts/podspec.sh
+++ scripts/podspec.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
-echo "1. Package name? (e.g. BVLinearGradient, it's the .xcodeproj's name)"
-read name
+echo "1. Package's name? (e.g. react-native-linear-gradient)"
+read package
 
-echo "2. Github url? (e.g. react-native-community/react-native-linear-gradient)"
+echo "2. Module's name? (e.g. BVLinearGradient, .xcodeproj's name)"
+read module
+
+echo "3. Github's url? (e.g. react-native-community/react-native-linear-gradient)"
 read url
 
-echo "3. node_modules's name? (e.g. react-native-linear-gradient)?"
-read path
+IFS='/' read -r -a author <<< "$url"
 
-/bin/cat <<EOM >"./node_modules/$path/$name.podspec"
+/bin/cat <<EOM >"./node_modules/$package/$package.podspec"
 require 'json'
 
 package = JSON.parse(File.read(File.join(__dir__, 'package.json')))
 
 Pod::Spec.new do |s|
-  s.name         = "$name"
+  s.name         = "$package"
   s.version      = package['version']
+  s.summary      = "$package's podspec"
 
-  s.authors      = { "$name" => "$name" }
+  s.authors      = { "${author[0]}" => "${author[0]}" }
   s.homepage     = "https://github.com/$url"
   s.license      = package['license']
   s.platform     = :ios, "8.0"
+
+  s.module_name  = "$module"
 
   s.source       = { :git => "https://github.com/$url.git", :tag => "v#{s.version}" }
   s.source_files  = "ios/**/*.{h,m}"
