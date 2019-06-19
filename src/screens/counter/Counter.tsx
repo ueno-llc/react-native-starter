70f648a4cc0559d382e666adcbd028c45da50244
Jérémy Barbet <jeremgraph@gmail.com>
2019-06-19 13:15:22 +0000

    Return counter screen

--- src/screens/counter/Counter.tsx
+++ src/screens/counter/Counter.tsx
@@ -5,37 +5,35 @@ import { Button } from '../../components/button/Button';
 import { Counter } from '../../stores/Counter';
 import s from './Counter.scss';
 
-export const CounterScreen = () => {
-  return (
-    <Observer
-      render={() => (
-        <View style={s.counter} testID="COUNTER_SCREEN">
-          <View style={s.counter__content}>
-            <Text style={s.counter__text}>Counter: {Counter.counter}</Text>
-          </View>
-
-          <View style={s.counter__actions}>
-            <Button
-              title="Decrement"
-              onPress={Counter.decrement}
-              testID="BUTTON_DECREMENT"
-              style={s.counter__button}
-            />
-
-            <View style={s.counter__spacer} />
-
-            <Button
-              title="Increment"
-              onPress={Counter.increment}
-              testID="BUTTON_INCREMENT"
-              style={s.counter__button}
-            />
-          </View>
+export const CounterScreen = () => (
+  <Observer
+    render={() => (
+      <View style={s.counter} testID="COUNTER_SCREEN">
+        <View style={s.counter__content}>
+          <Text style={s.counter__text}>Counter: {Counter.counter}</Text>
         </View>
-      )}
-    />
-  );
-};
+
+        <View style={s.counter__actions}>
+          <Button
+            title="Decrement"
+            onPress={Counter.decrement}
+            testID="BUTTON_DECREMENT"
+            style={s.counter__button}
+          />
+
+          <View style={s.counter__spacer} />
+
+          <Button
+            title="Increment"
+            onPress={Counter.increment}
+            testID="BUTTON_INCREMENT"
+            style={s.counter__button}
+          />
+        </View>
+      </View>
+    )}
+  />
+);
 
 CounterScreen.options = {
   topBar: {
